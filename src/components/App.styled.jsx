26394dc6422bee5d91a2658982904e214eed7dde
alexxx888 <alexxxusachev888@gmail.com>
2023-02-11 16:05:14 +0200

    additional fixes

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -3,9 +3,9 @@ import styled from '@emotion/styled';
 export const Container = styled.div`
     height: 100%;
     display: flex;
-    flexDirection : column;
-    justifyContent: center;
-    alignItems: center;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
     fontSize: 40;
     color: #010101;
     backgroundColor: #D7E1E9;
